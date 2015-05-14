51c6beab1805b2143cd2756b7c25ab766b9477c0
Jim Kang <jimkang@gmail.com>
2015-05-14 11:07:31 -0400

    Restorted ratings by making regexes case insensitive.

--- data/rating-specs.js
+++ data/rating-specs.js
@@ -1,90 +1,90 @@
 module.exports = [
   {
-    regex: /military|\Wwar\W|nuclear|nuke|\Warms\W|seized|capabilities|pentagon|drill|brinksmanship/,
+    regex: /military|\Wwar\W|nuclear|nuke|\Warms\W|seized|capabilities|pentagon|drill|brinksmanship/i,
     score: 5
   },
   {
-    regex: /bank|currenc|econom/,
+    regex: /bank|currenc|econom/i,
     score: 5
   },
   {
-    regex: /government/,
+    regex: /government/i,
     score: 5
   },
   {
-    regex: /students/,
+    regex: /students/i,
     score: 2
   },
   {
-    regex: /company|brokerage|investors/,
+    regex: /company|brokerage|investors/i,
     score: 2
   },
   {
-    regex: /leaders/,
+    regex: /leaders/i,
     score: 5
   },
   {
-    regex: /ant/,
+    regex: /ant/i,
     score: 1
   },
   {
-    regex: /chinese|iranian/,
+    regex: /chinese|iranian/i,
     score: 1
   },
   {
-    regex: /clinton|kerry/,
+    regex: /clinton|kerry/i,
     score: 2
   },
   {
-    regex: /obama/,
+    regex: /obama/i,
     score: 3
   },
   {
-    regex: /market|stock|import|export/,
+    regex: /market|stock|import|export/i,
     score: 5
   },
   {
-    regex: /bought|buying|buyer/,
+    regex: /bought|buying|buyer/i,
     score: 3
   },
   {
-    regex: /why|what|analysis|analyst/,
+    regex: /why|what|analysis|analyst/i,
     score: 2
   },
   {
-    regex: /illion|massive/,
+    regex: /illion|massive/i,
     score: 5
   },
   {
-    regex: /afraid|fear|mistrust|worry/,
+    regex: /afraid|fear|mistrust|worry/i,
     score: 5
   },
   {
-    regex: /secret|espionage/,
+    regex: /secret|espionage/i,
     score: 5
   },
   {
-    regex: /hardliner|policy/,
+    regex: /hardliner|policy/i,
     score: 5
   },
   {
-    regex: /power|dominance/,
+    regex: /power|dominance/i,
     score: 5
   },
   {
-    regex: /counterfeit/,
+    regex: /counterfeit/i,
     score: 5
   },
   {
-    regex: /cyber/,
+    regex: /cyber/i,
     score: 5
   },
   {
-    regex: /trafficking/,
+    regex: /trafficking/i,
     score: 2
   },
   {
-    regex: /talks|visit|summit|bilateral/,
+    regex: /talks|visit|summit|bilateral/i,
     score: 2
   }
 ];
